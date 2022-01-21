ef9c027adfa271ea064e6306262c1e3df4837fe5
Adham <muzaffarov.adham@gmail.com>
2022-01-21 10:10:09 +0500

    telegraf-i18n added

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+const path = require("path");
 const { Telegraf, Markup } = require("telegraf");
+const TelegrafI18n = require("telegraf-i18n");
+const { match, reply } = require("telegraf-i18n");
 const { TOKEN } = require("./configs/keys");
 const bot = new Telegraf(TOKEN);
+
+const i18n = new TelegrafI18n({
+    defaultLanguage: "uz",
+    directory: path.resolve(__dirname, "locales"),
+    useSession: true,
+    sessionName: "session",
+});
+
+// Middlewares
+bot.use(i18n.middleware());
 
 // Error Handling
 bot.catch((err, ctx) => {
     console.log(err);
-    return ctx.reply("Something Wrong");
+    return ctx.reply();
 });
 
 // Public
-bot.start((ctx) => {
-    ctx.reply("Bot started working...", {
-        parse_mode: "HTML",
-        disable_web_page_preview: true,
-    });
+bot.hears(match("keyboards.main_keyboard.search"), (ctx) => {
+    ctx.reply("good");
 });
 
 module.exports = bot;
